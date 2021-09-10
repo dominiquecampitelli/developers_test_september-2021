3a451ae24083a4bd6de28b65e05a55748495896b
Lais Campitelli <laiscamp@outlook.com>
2021-09-10 00:00:51 -0300

    added webpack and router dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>teste</h1>
+        </div>
+    );
 }
 
 export default App;
